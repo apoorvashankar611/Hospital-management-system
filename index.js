c1a1e556dbc28559f296c85986edd11103b5d576
Apoorva <apoorvashankar611@gmail.com>
2025-05-09 08:48:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-const express = require("express");
-const admin_router = require("./router/admin");
-const auth_router = require("./router/auth");
-const public_router = require("./router/public");
-const doctor_router = require("./router/doctor");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const morgan = require('morgan');
-require("dotenv").config();
-const mongoose_connection = require("./db/connection");
-const patient_router = require("./router/patient");
-const doctor = require("./model/doctor");
-const app = express();
-app.use(cors({credentials: true, origin: true}));
-mongoose_connection(app);
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(morgan('dev')); // 'dev' is the predefined format for Morgan, you can also use other formats
-app.use("/public",public_router);
-app.use(auth_router);
-app.use(admin_router);
-app.use(patient_router);
-app.use("/public",public_router)
-app.use(doctor_router);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-// {
-//     origin:"www.xyz.com",
-//     methods:['GET','POST','PUT','DELETE'],
-// }
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./store";
+import { Provider } from "react-redux";
+import { Toaster } from "react-hot-toast";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <Toaster/>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
+
+
